ffe2bb2c8e7ede80f5e655a03891988c4d723c1e
Haojun Lu <haojunlu@foxmail.com>
2020-06-06 21:20:24 -0700

    clean up py

--- gol_py/check_all_func.py
+++ gol_py/check_all_func.py
@@ -8,16 +8,30 @@ print(os.getcwd())
 import gol_py
 
 
-# todo move to cpp?
-def gen_coor(x,y):
+def gen_coor(x, y):
     sz = gol_py.CoorType()
-    sz.set( 0,  x)
-    sz.set( 1, y)
+    sz.set(0, x)
+    sz.set(1, y)
     return sz
 
+
+def extra_print(func):
+    def wrapper():
+        char_cnt = 60
+        div_char = '-'
+        print()
+        print(div_char * char_cnt)
+        func()
+        print(div_char * char_cnt)
+        print()
+
+    return wrapper
+
+
+@extra_print
 def t_playground():
     print("test playground...")
-    pl = gol_py.PlayGround(gen_coor(8,8))
+    pl = gol_py.PlayGround(gen_coor(8, 8))
     print(type(pl))
     num_iter = 5
     for i in range(num_iter):
@@ -29,18 +43,20 @@ def t_playground():
         print(len(coords), type(coords))
 
 
+@extra_print
 def t_grid():
     print("test grid...")
 
-    #grid = gol_py.Grid2DVec(gen_coor(4,4))
-    grid = gol_py.Grid2DVectorPtr(gen_coor(4,4))
+    # grid = gol_py.Grid2DVec(gen_coor(4,4))
+    grid = gol_py.Grid2DVectorPtr(gen_coor(4, 4))
     print(grid.sum(), grid.get_digest())
-    grid.set_cell(gen_coor(0,0), 1)
+    grid.set_cell(gen_coor(0, 0), 1)
     print(grid.sum(), grid.get_digest())
     coords = grid.get_live_cells()
     print(len(coords), type(coords))
 
 
+@extra_print
 def t_coord():
     print("test coord...")
     co = gol_py.CoorType()
@@ -51,14 +67,10 @@ def t_coord():
     print("Value should be changed: {} {}".format(co.get(0), co.get(1)))
 
 
-
 def main():
     t_coord()
-    print()
     t_grid()
-    print()
     t_playground()
-    print()
 
 
 if __name__ == '__main__':

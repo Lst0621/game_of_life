e8a124c84560173984d8a725de6d8153e8c5a8a1
Haojun Lu <haojunlu@foxmail.com>
2020-06-06 20:59:35 -0700

    add stack dump

--- gol_qt/main.cpp
+++ gol_qt/main.cpp
@@ -1,8 +1,10 @@
+#include <glog/logging.h>
 #include <iostream>
 #include "GolWidget.h"
 using namespace lst::gol;
 
 int main(int argc, char *argv[]) {
+    google::InstallFailureSignalHandler();
     std::cout << "Game of Life" << std::endl;
     QApplication app(argc, argv);
 

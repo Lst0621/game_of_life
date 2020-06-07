f3be95626d5c7d25f8b81d41244b79e484ada88f
Haojun Lu <haojunlu@foxmail.com>
2020-06-06 20:47:23 -0700

    update py for template

--- gol_py/check_all_func.py
+++ gol_py/check_all_func.py
@@ -2,47 +2,64 @@
 # cwd is set to ${CMAKE_CURRENT_BINARY_DIR
 import os
 import sys
+
 sys.path.append(os.getcwd())
 print(os.getcwd())
 import gol_py
 
 
+# todo move to cpp?
+def gen_coor(x,y):
+    sz = gol_py.CoorType()
+    sz.set( 0,  x)
+    sz.set( 1, y)
+    return sz
+
 def t_playground():
     print("test playground...")
-    pl = gol_py.PlayGround(8,8)
+    pl = gol_py.PlayGround(gen_coor(8,8))
     print(type(pl))
-    num_iter = 4
+    num_iter = 5
     for i in range(num_iter):
         pl.run()
         grid = pl.getGrid()
-        print(grid.sum(),grid.get_digest())
+        print(type(grid))
+        print(grid.sum(), grid.get_digest())
         coords = grid.get_live_cells()
-        print(len(coords),type(coords))
-    print()
+        print(len(coords), type(coords))
 
 
 def t_grid():
     print("test grid...")
-    grid = gol_py.Grid(4,4)
-    print(grid.sum(),grid.get_digest())
-    grid.set_cell(0,0,1)
-    print(grid.sum(),grid.get_digest())
-    print()
+
+    #grid = gol_py.Grid2DVec(gen_coor(4,4))
+    grid = gol_py.Grid2DVectorPtr(gen_coor(4,4))
+    print(grid.sum(), grid.get_digest())
+    grid.set_cell(gen_coor(0,0), 1)
+    print(grid.sum(), grid.get_digest())
+    coords = grid.get_live_cells()
+    print(len(coords), type(coords))
+
 
 def t_coord():
-    print("test coord")
+    print("test coord...")
     co = gol_py.CoorType()
-    print("Coord dim: {} {}".format(len(co),co.size()))
-    print( "Value at start: {} {}".format(co.get(0),co.get(1)))
-    co.set(0,1)
-    co.set(1,1)
-    print( "Value should be changed: {} {}".format(co.get(0),co.get(1)))
-    print()
+    print("Coord dim: {} {}".format(len(co), co.size()))
+    print("Value at start: {} {}".format(co.get(0), co.get(1)))
+    co.set(0, 1)
+    co.set(1, 1)
+    print("Value should be changed: {} {}".format(co.get(0), co.get(1)))
+
+
 
 def main():
     t_coord()
+    print()
     t_grid()
+    print()
     t_playground()
+    print()
+
 
 if __name__ == '__main__':
     main()
--- gol_py/gol_py.cpp
+++ gol_py/gol_py.cpp
@@ -3,6 +3,8 @@
 #include <boost/python/suite/indexing/indexing_suite.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 #include "Playground.h"
+
+#include "gol_py.h"
 using namespace boost::python;
 using namespace lst::gol;
 
@@ -10,35 +12,43 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(playground_run_overload, PlayGround::run,
                                        0, 1)
 
 // cannot find a way to directly use at or [] func yet
-int coor_get(Grid::CoorType& coor, size_t i) {
-    return coor[i];
+using Grid2DVector = VectorGrid<2>;
+using Grid2DBase = Grid<2>;
+using Grid2DVectorPtrWrapper = GridImplPt<Grid2DVector>;
+using PlayGround2D = PlayGround<2>;
+
+int coor_get(Grid2DBase::CoorType& coor, size_t pos) {
+    return coor[pos];
 }
-void coor_set(Grid::CoorType& coor, size_t i, int value) {
-    coor[i] = value;
+void coor_set(Grid2DBase::CoorType& coor, size_t pos, int value) {
+    coor[pos] = value;
 }
 
-// std::function<const int&(size_t)> fooooo = &bar::at;
 BOOST_PYTHON_MODULE(gol_py) {
-    class_<Grid::CoorType>("CoorType")
-        .def("size", &Grid::CoorType::size)
-        .def("__len__", &Grid::CoorType::size)
+    class_<Grid2DBase::CoorType>("CoorType")
+        .def("size", &Grid<2>::CoorType::size)
+        .def("__len__", &Grid<2>::CoorType::size)
         .def("get", coor_get)
         .def("set", coor_set);
 
-    class_<std::vector<Grid::CoorType>>("CordList")
-        .def((vector_indexing_suite<std::vector<Grid::CoorType>>()));
+    class_<std::vector<Grid2DBase::CoorType>>("CordList")
+        .def((vector_indexing_suite<std::vector<Grid2DVector::CoorType>>()));
 
-    class_<Grid>("Grid", init<int, int>())
-        .def("get_digest", &Grid::get_digest)
-        .def("sum", &Grid::sum)
-        .def("get_live_cells", &Grid::get_live_cells,
+    class_<Grid2DBase, std::shared_ptr<Grid2DBase>, boost::noncopyable>(
+        "Grid2DBasePtr", no_init);
+
+    class_<Grid2DVector, bases<Grid2DBase>, std::shared_ptr<Grid2DVector>>(
+        "Grid2DVectorPtr", init<Grid2DVector::CoorType>())
+        .def("sum", &Grid2DVectorPtrWrapper::sum)
+        .def("get_digest", &Grid2DVectorPtrWrapper::get_digest)
+        .def("get_live_cells", &Grid2DVectorPtrWrapper::get_live_cells,
              boost::python::return_value_policy<
                  boost::python::return_by_value>())
-        .def("set_cell", &Grid::set_cell)
-        .def("evolve", &Grid::evolve);
+        .def("set_cell", &Grid2DVectorPtrWrapper::set_cell)
+        .def("evolve", &Grid2DVectorPtrWrapper::evolve);
 
-    class_<PlayGround>("PlayGround", init<int, int>())
-        .def("set_up", &PlayGround::set_up)
-        .def("getGrid", &PlayGround::getGrid, return_internal_reference<>())
-        .def("run", &PlayGround::run, playground_run_overload());
+    class_<PlayGround2D>("PlayGround", init<Grid2DVector::CoorType>())
+        .def("set_up", &PlayGround2D::set_up)
+        .def("getGrid", &PlayGround2D::getGrid)
+        .def("run", &PlayGround2D::run, playground_run_overload());
 }
